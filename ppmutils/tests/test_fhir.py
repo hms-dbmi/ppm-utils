a4019c5742702a84f04757397b574653dcf5994b
Bryan Larson <b32147@gmail.com>
2019-02-08 12:15:41 -0500

    Added tests

---
+++ ppmutils/tests/test_fhir.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestFHIR(TestCase):
+    def test_is_true(self):
+        self.assertTrue(True)
+
+    def test_is_false(self):
+        self.assertFalse(False)
